8df0305dd15b7b9310c18c081bdcfb1b40744d1a
Miguel Ángel García <miguelangel.garcia@gmail.com>
2011-02-03 13:04:42 +0800

    green. Eliminando los valores multi-letra

--- magmax/python/romannumerals.py
+++ magmax/python/romannumerals.py
@@ -4,18 +4,21 @@
 class ArabianToRoman:
     _EQUIVALENCES = [
         (1000, 'M'),
-        (900, 'CM'),
         (500, 'D'),
-        (400, 'CD'),
         (100, 'C'),
-        (90, 'XC'),
         (50, 'L'),
-        (40, 'XL'),
         (10, 'X'),
-        (9, 'IX'),
         (5, 'V'),
-        (4, 'IV'),
         (1, 'I')
+        ]
+
+    _EQUIVALENCES_MULTI = [
+        (900, 'CM'),
+        (400, 'CD'),
+        (90, 'XC'),
+        (40, 'XL'),
+        (9, 'IX'),
+        (4, 'IV'),
         ]
 
     def __init__(self):
@@ -31,10 +34,26 @@ class ArabianToRoman:
         return self.translated
 
     def __apply_all (self):
-        for k, v in self._EQUIVALENCES:
-            self.__apply (k, v)
+        pos = 0
+        last_value = 0
+        while self.arabian > 0:
+            if last_value == self.arabian:
+                pos += 1
+            last_value = self.arabian
+            self.__apply (*self._EQUIVALENCES[pos])
+            self.__apply_multiletter (pos)
 
     def __apply (self, arabian, roman):
         while self.arabian >= arabian:
-            self.arabian -= arabian
-            self.translated += roman
+            self.__add_value ( arabian, roman)
+
+    def __apply_multiletter (self, pos):
+        if pos >= len (self._EQUIVALENCES_MULTI):
+            return
+        arabian, roman = self._EQUIVALENCES_MULTI[pos]
+        if self.arabian >= arabian:
+            self.__add_value ( arabian, roman)
+
+    def __add_value ( self, arabian, roman ):
+        self.arabian -= arabian
+        self.translated += roman
