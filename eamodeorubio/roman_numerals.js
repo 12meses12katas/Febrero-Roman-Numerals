4c95ff785ba5dddf7ac0ff59b18de95e2ad81d4a
Enrique J. Amodeo Rubio <eamodeorubio@gmail.com>
2011-02-05 04:16:29 +0800

    Elaborating the funny idea with more tests. You can see the duplication in the code but a pattern is revealing

--- eamodeorubio/roman_numerals.js
+++ eamodeorubio/roman_numerals.js
@@ -13,9 +13,23 @@ var RomanNumber=function(value) {
 		var r=romanNumeralsByValue[value];
 		if(r)
 			return r;
-		if(Math.abs(value-5)<Math.abs(value-1))
+		
+		// 50 / 10 / 5
+		if(Math.abs(value-50)<(Math.abs(value-10-5)))
+			return new RomanNumber(value-50).toString()+'L';
+		if(value>10)
+			return 'X'+new RomanNumber(value-10).toString();
+		
+		// 10 / 5 / 1
+		if(Math.abs(value-10)<(Math.abs(value-5-1)))
+			return new RomanNumber(value-10).toString()+'X';
+		if(value>5)
+			return 'V'+new RomanNumber(value-5).toString();
+		
+		// 5 / 1 / 0
+		if(Math.abs(value-5)<Math.abs(value-1-0))
 			return new RomanNumber(value-5).toString()+'V';
-		else
+		if(value>1)
 			return 'I'+new RomanNumber(value-1).toString();
 	};
 };
