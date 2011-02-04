247a18cfd7b0328e48dfa592197d091d5141200e
Enrique J. Amodeo Rubio <eamodeorubio@gmail.com>
2011-02-05 03:11:28 +0800

    Second test, the V

--- eamodeorubio/roman_numerals.js
+++ eamodeorubio/roman_numerals.js
@@ -2,5 +2,7 @@ var RomanNumber=function(value) {
 	this.toString=function() {
 		if(value==1)
 			return "I";
+		if(value==5)
+			return "V";
 	};
 };
--- eamodeorubio/roman_numerals_spec.js
+++ eamodeorubio/roman_numerals_spec.js
@@ -3,5 +3,9 @@ describe("RomanNumber", function() {
 		it("1 is converted to I", function() {
 			expect(new RomanNumber(1).toString()).toEqual("I");
 		});
+		
+		it("5 is converted to V", function() {
+			expect(new RomanNumber(5).toString()).toEqual("V");
+		});
 	});
 });
