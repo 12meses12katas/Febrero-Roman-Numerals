0ef299d9b71a06b69f90f4ce9c0223bb841ab238
Enrique J. Amodeo Rubio <eamodeorubio@gmail.com>
2011-02-07 06:15:33 +0800

    Separated logic to create the 'virtual' roman numeral using the 'substract rule'

--- eamodeorubio/roman_numerals.js
+++ eamodeorubio/roman_numerals.js
@@ -9,6 +9,19 @@ var RomanNumber=function(value) {
 		1000, 'M'
 	];
 	var romanNumeralsCount=romanNumeralsByValue.length;
+	var giveRomanNumeralAndArabicConversionForSubstractRuleOf=function(i) {
+		var romanNumeral=romanNumeralsByValue[i];
+		var arabicNumeral=romanNumeralsByValue[i-1];
+		var substractiveRomanNumeral=romanNumeralsByValue[i-2];
+		var substractiveArabicNumeral=romanNumeralsByValue[i-3];
+		var valueOfSubstractRule=arabicNumeral-substractiveArabicNumeral;
+		if(valueOfSubstractRule<=substractiveArabicNumeral) {
+			substractiveRomanNumeral=romanNumeralsByValue[i-4];
+			substractiveArabicNumeral=romanNumeralsByValue[i-5];
+			valueOfSubstractRule=arabicNumeral-substractiveArabicNumeral;
+		}
+		return [substractiveRomanNumeral+romanNumeral, valueOfSubstractRule];
+	};
 	
 	this.toString=function() {
 		if(value==0)
@@ -18,16 +31,9 @@ var RomanNumber=function(value) {
 			var arabicNumeral=romanNumeralsByValue[i-1];
 			var restOfTheValue=value-arabicNumeral;
 			if (restOfTheValue<0 && i >= 3) {
-				var substractiveRomanNumeral=romanNumeralsByValue[i-2];
-				var substractiveArabicNumeral=romanNumeralsByValue[i-3];
-				var valueOfSubstractRule=arabicNumeral-substractiveArabicNumeral;
-				if(valueOfSubstractRule<=substractiveArabicNumeral) {
-					substractiveRomanNumeral=romanNumeralsByValue[i-4];
-					substractiveArabicNumeral=romanNumeralsByValue[i-5];
-					valueOfSubstractRule=arabicNumeral-substractiveArabicNumeral;
-				}
-				romanNumeral=substractiveRomanNumeral+romanNumeral;
-				restOfTheValue=value-valueOfSubstractRule;
+				var virtualRomanNumeralAndArabicConversion=giveRomanNumeralAndArabicConversionForSubstractRuleOf(i);
+				romanNumeral=virtualRomanNumeralAndArabicConversion[0];
+				restOfTheValue=value-virtualRomanNumeralAndArabicConversion[1];
 			}
 			if(restOfTheValue>=0)
 					return romanNumeral+new RomanNumber(restOfTheValue).toString();
