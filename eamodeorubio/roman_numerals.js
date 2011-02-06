5e6978ac59af40c0e52bbef1fda2d4da909b5520
Enrique J. Amodeo Rubio <eamodeorubio@gmail.com>
2011-02-07 07:14:55 +0800

    Reversed order of the conversions table and some names changed. Now we can use forward loops that I found more legible than reversed ones

--- eamodeorubio/roman_numerals.js
+++ eamodeorubio/roman_numerals.js
@@ -1,42 +1,44 @@
 var RomanNumber=function(value) {
 	var originalRomanNumeral2ArabicConversions=[
-		{'arabic':1, 'roman':'I'},
-		{'arabic':5, 'roman':'V'},
-		{'arabic':10, 'roman':'X'},
-		{'arabic':50, 'roman':'L'},
-		{'arabic':100, 'roman':'C'},
+		{'arabic':1000, 'roman':'M'},
 		{'arabic':500, 'roman':'D'},
-		{'arabic':1000, 'roman':'M'}
+		{'arabic':100, 'roman':'C'},
+		{'arabic':50, 'roman':'L'},
+		{'arabic':10, 'roman':'X'},
+		{'arabic':5, 'roman':'V'},
+		{'arabic':1, 'roman':'I'}
 	];
+	var originalRoman2ArabicConversionsCount=originalRomanNumeral2ArabicConversions.length;
+	
 	var romanNumeral2ArabicConversions=[];
 	
-	var giveRomanNumeralAndArabicConversionForSubstractRuleOf=function(i) {
-		if(i<1)
+	var giveRoman2ArabicConversionForSubstractRuleOf = function(i){
+		if (i == (originalRoman2ArabicConversionsCount-1)) 
 			return;
-		var roman2Arabic=originalRomanNumeral2ArabicConversions[i];
-		var substractiveRoman2Arabic=originalRomanNumeral2ArabicConversions[i-1];
-		if(roman2Arabic.arabic<=(2*substractiveRoman2Arabic.arabic))
-			substractiveRoman2Arabic=originalRomanNumeral2ArabicConversions[i-2];
-		romanNumeral2ArabicConversions.unshift({
-			'arabic':(roman2Arabic.arabic-substractiveRoman2Arabic.arabic),
-			'roman':(substractiveRoman2Arabic.roman+roman2Arabic.roman)
+		var roman2Arabic = originalRomanNumeral2ArabicConversions[i];
+		var substractiveRoman2Arabic = originalRomanNumeral2ArabicConversions[i + 1];
+		if (roman2Arabic.arabic <= (2 * substractiveRoman2Arabic.arabic)) 
+			substractiveRoman2Arabic = originalRomanNumeral2ArabicConversions[i + 2];
+		romanNumeral2ArabicConversions.push({
+			'arabic': (roman2Arabic.arabic - substractiveRoman2Arabic.arabic),
+			'roman': (substractiveRoman2Arabic.roman + roman2Arabic.roman)
 		});
 	};
 	
-	for (var i = (originalRomanNumeral2ArabicConversions.length - 1); i >= 0; i--) {
-		romanNumeral2ArabicConversions.unshift(originalRomanNumeral2ArabicConversions[i]);
-		giveRomanNumeralAndArabicConversionForSubstractRuleOf(i);
+	for (var i = 0; i < originalRoman2ArabicConversionsCount; i++) {
+		romanNumeral2ArabicConversions.push(originalRomanNumeral2ArabicConversions[i]);
+		giveRoman2ArabicConversionForSubstractRuleOf(i);
 	}
 	
-	var romanNumeralsCount=romanNumeral2ArabicConversions.length;
+	var roman2ArabicConversionsCount=romanNumeral2ArabicConversions.length;
 	
 	this.toString=function() {
 		if(value==0)
 			return '';
-		for(var i=(romanNumeralsCount-1);i>=0;i--) {
-			var roman2Arabic=romanNumeral2ArabicConversions[i];
-			if(value>=roman2Arabic.arabic)
-					return roman2Arabic.roman+new RomanNumber(value-roman2Arabic.arabic).toString();
+		for (var i = 0; i < roman2ArabicConversionsCount; i++) {
+			var roman2Arabic = romanNumeral2ArabicConversions[i];
+			if (value >= roman2Arabic.arabic) 
+				return roman2Arabic.roman + new RomanNumber(value - roman2Arabic.arabic).toString();
 		}
 	};
 };
