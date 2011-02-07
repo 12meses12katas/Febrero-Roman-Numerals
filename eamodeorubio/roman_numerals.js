782ce8b2a3fed2d71ea7e98fe318219d4c3534cb
Enrique J. Amodeo Rubio <eamodeorubio@gmail.com>
2011-02-08 00:22:23 +0800

    Optimizations. Instead of iterating to find the roman numeral we use a hash.

--- eamodeorubio/roman_numerals.js
+++ eamodeorubio/roman_numerals.js
@@ -2,6 +2,7 @@ var RomanNumber=(function(originalRomanNumeral2ArabicConversions) {
 	var originalRoman2ArabicConversionsCount=originalRomanNumeral2ArabicConversions.length;
 	
 	var romanNumeral2ArabicConversions=[];
+	var arabicNumbersByRomanNumeral={};
 	
 	var giveRoman2ArabicConversionForSubstractRuleOf = function(i){
 		if (i == (originalRoman2ArabicConversionsCount-1)) 
@@ -10,14 +11,18 @@ var RomanNumber=(function(originalRomanNumeral2ArabicConversions) {
 		var substractiveRoman2Arabic = originalRomanNumeral2ArabicConversions[i + 1];
 		if (roman2Arabic.arabic <= (2 * substractiveRoman2Arabic.arabic)) 
 			substractiveRoman2Arabic = originalRomanNumeral2ArabicConversions[i + 2];
-		romanNumeral2ArabicConversions.push({
+		var roman2ArabicConversion={
 			'arabic': (roman2Arabic.arabic - substractiveRoman2Arabic.arabic),
 			'roman': (substractiveRoman2Arabic.roman + roman2Arabic.roman)
-		});
+		};
+		romanNumeral2ArabicConversions.push(roman2ArabicConversion);
+		arabicNumbersByRomanNumeral[roman2ArabicConversion.roman]=roman2ArabicConversion.arabic;
 	};
 	
 	for (var i = 0; i < originalRoman2ArabicConversionsCount; i++) {
-		romanNumeral2ArabicConversions.push(originalRomanNumeral2ArabicConversions[i]);
+		var roman2ArabicConversion=originalRomanNumeral2ArabicConversions[i];
+		romanNumeral2ArabicConversions.push(roman2ArabicConversion);
+		arabicNumbersByRomanNumeral[roman2ArabicConversion.roman]=roman2ArabicConversion.arabic;
 		giveRoman2ArabicConversionForSubstractRuleOf(i);
 	}
 	
@@ -37,13 +42,13 @@ var RomanNumber=(function(originalRomanNumeral2ArabicConversions) {
 		this.toNumber = function() {
 			if(value.length==0)
 				return 0;
-			var roman2Arabic;
-			for (var i = 0; !roman2Arabic && i < roman2ArabicConversionsCount; i++) {
-				var currentRoman2Arabic = romanNumeral2ArabicConversions[i];
-				if(value.indexOf(currentRoman2Arabic.roman)==0)
-					roman2Arabic=currentRoman2Arabic;
+			var roman=value.substr(0,2);
+			var arabic=arabicNumbersByRomanNumeral[roman];
+			if(typeof(arabic)!='number') {
+				roman=value[0];
+				arabic=arabicNumbersByRomanNumeral[roman];
 			}
-			return roman2Arabic.arabic + new RomanNumber(value.substr(roman2Arabic.roman.length)).toNumber();
+			return arabic + new RomanNumber(value.substr(roman.length)).toNumber();
 		};
 	};
 })([
