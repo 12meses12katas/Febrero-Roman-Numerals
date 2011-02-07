b75caa00dbd7dc288f6bca7ec3f0aecd250caa54
Alfredo Casado <casado.alfredo@gmail.com>
2011-02-07 10:01:00 +0800

    rspec, textmate y git funcionando. Por tengo un entorno decente para ruby en el peacho mac nuevo

--- AlfredoCasado/prueba.rb
+++ AlfredoCasado/prueba.rb
@@ -1,7 +1,8 @@
 require "rspec"
 
-describe "kdkdk" do
-  it "kdkdkd" do
-    
+describe "roman to arabic number converter" do
+  
+  it "transform arabic numbers to simple roman numbers" do
+    "I".to_arabic.should == "I"
   end
 end
