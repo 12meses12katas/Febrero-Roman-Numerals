e204918978256991e8971a2c7081b2bb7942e593
ximotur <ximotur@gmail.com>
2011-02-09 20:46:10 +0800

    Reorder code, agrupate and reduce lines and, as a result, obfuscate code

--- joatumas/kata_febrer.c
+++ joatumas/kata_febrer.c
@@ -12,11 +12,39 @@ int add_letter(char letter, char *out_str, int n){
 	return(len + i);
 }
 
+int numeral_to_roman(int num, char *str_out){
+	int i;
+	char symbols[7] = {'M','D', 'C', 'L', 'X', 'V', 'I'};
+	int divided_num[7];
+	//First, divide out the number into interesting divisors:
+	divided_num[0] = num / 1000; //1000
+	divided_num[1] = (num % 1000)/500; //500
+	divided_num[2] = (num % 500)/100; //100
+	divided_num[3] = (num % 100)/50;//50
+	divided_num[4] = (num % 50)/10;//10
+	divided_num[5] = (num % 10)/5;//5
+	divided_num[6] = num%5;//1
+	//Begin adding letters to the end of the string:
+	add_letter(symbols[0], str_out, divided_num[0]);
+	for(i=1; i<7; i = i+2){
+		if(divided_num[i+1] == 4){
+			add_letter(symbols[i+1], str_out, 1);
+			if(divided_num[i])
+				add_letter(symbols[i-1], str_out, 1);
+			else
+				add_letter(symbols[i], str_out, 1);
+			divided_num[i] = divided_num[i+1] = 0;
+		}
+		add_letter(symbols[i], str_out, divided_num[i]);
+		add_letter(symbols[i+1], str_out, divided_num[i+1]);
+	}
+	return(0);
+}
+
 int main(int argc, char **argv){
 	int num;
-	int unit, five, dec, fifty, cent, fivehund, mil;
 	char str_out[20];
-	num = unit = dec = cent = mil = five = fifty = fivehund = str_out[0] = 0;
+	str_out[0] = 0;
 	if(argc != 2){
 		fprintf(stderr,"ERROR: Wrong arg numbers\n\tUSAGE: %s number\n", argv[0]);
 		exit(-1);
@@ -29,47 +57,7 @@ int main(int argc, char **argv){
 		fprintf(stderr,"ERROR: Please, do NOT exceed the number 3000\n");
 		exit(-1);
 	}
-	//First, divide out the number into interesting divisors:
-	mil = num / 1000;
-	fivehund = (num % 1000)/500;
-	cent = (num % 500)/100;
-	fifty = (num % 100)/50;
-	dec = (num % 50)/10;
-	five = (num % 10)/5;
-	unit = num%5;
-
-	//Begin adding letters to the end of the string:
-	add_letter('M', str_out, mil);
-	if(cent == 4){
-		add_letter('C', str_out, 1);
-		if(fivehund)
-			add_letter('M', str_out, 1);
-		else
-			add_letter('D', str_out, 1);
-		cent = fivehund = 0;
-	}
-	add_letter('D', str_out, fivehund);
-	add_letter('C', str_out, cent);
-	if(dec == 4){
-		add_letter('X', str_out, 1);
-		if(fifty)
-			add_letter('C', str_out, 1);
-		else
-			add_letter('L', str_out, 1);
-		fifty = dec = 0;
-	}
-	add_letter('L', str_out, fifty);
-	add_letter('X', str_out, dec);
-	if(unit == 4){
-		add_letter('I', str_out, 1);
-		if(five)
-			add_letter('X',str_out, 1);
-		else
-			add_letter('V',str_out, 1);
-		five = unit = 0;
-	}
-	add_letter('V', str_out, five);
-	add_letter('I', str_out, unit);
+	numeral_to_roman(num, str_out);
 	printf("%s\n", str_out);
 	exit(0);
 }
