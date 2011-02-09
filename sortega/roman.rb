e93387024845da64728889e1bd1b7cd8a1fe9822
Sebastian Ortega <sortega@netleading.com>
2011-02-09 15:48:37 +0100

    sortega: resuelto

--- sortega/roman.rb
+++ sortega/roman.rb
@@ -7,7 +7,7 @@ class Roman
     roman_digits = [
       ['', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX'],
       ['', 'X', 'XX', 'XXX', 'XL', 'L', 'LX', 'LXX', 'LXXX', 'XL'],
-      ['', 'C', 'CC', 'CCC', 'CM', 'M', 'MC', 'MCC', 'MCCC', 'CM'],
+      ['', 'C', 'CC', 'CCC', 'CD', 'D', 'DC', 'DCC', 'DCCC', 'CM'],
       ['', 'M', 'MM', 'MMM'],
     ]
 
@@ -21,5 +21,35 @@ class Roman
     end
 
     return roman
+  end
+
+  def Roman.to_i(roman)
+    numerals = {
+      'I' => 1,   'V' => 5,
+      'X' => 10,  'L' => 50,
+      'C' => 100, 'D' => 500,
+      'M' => 1000
+    }
+
+    values = []
+    roman.each_char { |ch| values.push numerals[ch] }
+
+    return aggregate(values)
+  end
+  
+  private
+
+  def Roman.aggregate(values)
+    return 0 if values.empty?
+    return values[0] if values.length == 1
+
+    first  = values.shift
+    second = values.first
+    if first < second
+      values.drop(1)
+      return second - first + aggregate(values)
+    else
+      return first + aggregate(values)
+    end
   end
 end
--- sortega/test_roman.rb
+++ sortega/test_roman.rb
@@ -10,4 +10,18 @@ class TestRoman < Test::Unit::TestCase
     assert_equal('MMVIII', Roman.to_roman(2008))
     assert_equal('MMM', Roman.to_roman(3000))
   end
+
+  def test_to_i
+    assert_equal(0, Roman.to_i(''))
+    assert_equal(1, Roman.to_i('I'))
+    assert_equal(3, Roman.to_i('III'))
+    assert_equal(10, Roman.to_i('X'))
+    assert_equal(510, Roman.to_i('DX'))
+  end
+
+  def test_roundtrip
+    for i in 1..3000
+      assert_equal(i, Roman.to_i(Roman.to_roman(i)))
+    end
+  end
 end
