5805099e2ae166368ad5f53bd299969e8e04e7ec
txingo <txingo@someserver.es>
2011-02-21 23:49:32 +0800

    (groovy) Segundo intento. Implementada la primera parte

--- txingo/RomanNumerals.groovy
+++ txingo/RomanNumerals.groovy
@@ -1,69 +1,26 @@
 
-
 class RomanNumerals {
-    private Map<String, Integer> ROMAN_TO_NUMERAL_MAP = ["I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000]
-    private Map<Integer, String> NUMERAL_TO_ROMAN_MAP = [1:"I", 5:"V", 10:"X", 50:"L", 100:"C", 500:"D", 1000:"M"]
     
+    /* de numeros romanos a enteros */
     int toNumeral(String roman){
         int sum = 0
-        roman.eachWithIndex { romanDigit, idx ->
-            int intValue = ROMAN_TO_NUMERAL_MAP.getAt(romanDigit)
-            int nextValue = getNextIntValue(roman, idx)
-            if(nextValue > intValue){
-                intValue *= -1
-            }
-            sum += intValue
+        roman.eachWithIndex { digit, idx ->
+            def digitValue = getDigitAt(roman, idx)
+            def nextDigitValue = getDigitAt(roman, idx + 1)
+            def sign = (digitValue < nextDigitValue) ? -1 : +1
+            sum += (sign * digitValue)
         }
         
-        return sum
+        sum
     }
     
-    private getNextIntValue(String roman, int idx){
-        if(idx + 1 < roman.count){
-            String romanDigit = roman.getAt (idx + 1)
-            return ROMAN_TO_NUMERAL_MAP.getAt(romanDigit)
-        }
-        return Integer.MIN_VALUE
+    private getDigitAt(def roman, def idx){
+        def ROMAN_TO_NUMERAL_MAP = ["I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000]
+        (idx < roman.count) ? ROMAN_TO_NUMERAL_MAP[roman[idx]] : Integer.MIN_VALUE
     }
     
+    /* de enteros a numeros romanos */
     String toRoman(int numeral){
-        String result = ""
-        def numbers = decompose(numeral)
-        numbers.each { number ->
-            result += NUMERAL_TO_ROMAN_MAP[number]
-        } 
-        return result
-    }
-    
-    private List decompose(int numeral){
-        def numList = []
-        
-        def values = NUMERAL_TO_ROMAN_MAP.collect { it.key }
-        
-        while(numeral > 0){
-            int n = values.size() - 1
-            int greater = values[n]
-            int substracter = getSubstracter(greater)
-            int smaller = greater - substracter
-            
-            if(numeral >= greater){
-                numList << greater
-                numeral -= greater
-            }else if(numeral >= smaller){
-                numList << substracter << greater
-                numeral -= smaller
-            } else {
-                values.remove(n)
-            }
-        }
-        
-        numList
-    }
-    
-    private int getSubstracter(int greater){
-        if(greater > 100) return 100
-        if(greater > 10) return 10
-        if(greater > 1) return 1
-        return 0
+        //TODO segunda parte
     }
 }
