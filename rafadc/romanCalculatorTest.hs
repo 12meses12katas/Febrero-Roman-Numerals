6eb66bd64fbf82cf7e0c4f58eb7d0a7a0f80e1fe
Rafa de Castro <rafadecastro@gmail.com>
2011-02-24 03:06:01 +0800

    Grouped tests

--- rafadc/romanCalculatorTest.hs
+++ rafadc/romanCalculatorTest.hs
@@ -12,8 +12,12 @@ testConversionOf2 = TestCase $ assertEqual
 testConversionOf3 = TestCase $ assertEqual 
   "Should get III from number 3" "III" ( fromNumberToRoman 3)
 
+testsForI = TestList [testConversionOf1, testConversionOf2, testConversionOf3]
+
 testConversionOf4 = TestCase $ assertEqual 
   "Should get IV from number 4" "IV" ( fromNumberToRoman 4)
+
+testsForIV = testConversionOf4
 
 testConversionOf5 = TestCase $ assertEqual 
   "Should get V from number 5" "V" ( fromNumberToRoman 5)
@@ -21,8 +25,12 @@ testConversionOf5 = TestCase $ assertEqual
 testConversionOf7 = TestCase $ assertEqual 
   "Should get VII from number 7" "VII" ( fromNumberToRoman 7)
 
+testsForV = TestList [testConversionOf5, testConversionOf7]
+
 testConversionOf9 = TestCase $ assertEqual 
   "Should get IX from number 9" "IX" ( fromNumberToRoman 9)
+
+testsForIX = testConversionOf9
 
 testConversionOf12 = TestCase $ assertEqual 
   "Should get XII from number 12" "XII" ( fromNumberToRoman 12)
@@ -30,14 +38,22 @@ testConversionOf12 = TestCase $ assertEqual
 testConversionOf14 = TestCase $ assertEqual 
   "Should get XIV from number 14" "XIV" ( fromNumberToRoman 14)
 
+testsForX = TestList [testConversionOf12, testConversionOf14]
+
 testConversionOf41 = TestCase $ assertEqual 
   "Should get XLI from number 41" "XLI" ( fromNumberToRoman 41)
+
+testsForXL = testConversionOf41
 
 testConversionOf53= TestCase $ assertEqual 
   "Should get LIII from number 53" "LIII" ( fromNumberToRoman 53)
 
+testsForL = testConversionOf53
+
 testConversionOf95= TestCase $ assertEqual 
   "Should get XCV from number 95" "XCV" ( fromNumberToRoman 95)
+
+testsForXC = testConversionOf95
 
 testConversionOf39= TestCase $ assertEqual 
   "Should get XXXIX from number 39" "XXXIX" ( fromNumberToRoman 39)
@@ -48,6 +64,9 @@ testConversionOf49= TestCase $ assertEqual
 testConversionOf89= TestCase $ assertEqual 
   "Should get LXXXIX from number 89" "LXXXIX" ( fromNumberToRoman 89)
 
-main = runTestTT $ TestList [testConversionOf1, testConversionOf2, testConversionOf3, testConversionOf4, testConversionOf5,
-							 testConversionOf7, testConversionOf9, testConversionOf12, testConversionOf14, testConversionOf39,
-							 testConversionOf41, testConversionOf49, testConversionOf53, testConversionOf89, testConversionOf95]
+testsForLimitCases = TestList [testConversionOf39, testConversionOf49, testConversionOf89] 
+
+
+main = runTestTT $ TestList [testsForI, testsForIV, testsForV, 
+							 testsForIX, testsForX, testsForXL, testsForL,
+							 testsForXC, testsForLimitCases]
