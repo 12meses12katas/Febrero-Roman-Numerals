b8ebbc31759f8d59742ac48160ed91634abed238
txingo <txingo@someserver.es>
2011-02-24 23:34:57 +0800

    (groovy) Segunda parte, utilizando una tabla que incluye 4's y 9's

--- txingo/RomanNumerals.groovy
+++ txingo/RomanNumerals.groovy
@@ -21,6 +21,16 @@ class RomanNumerals {
     
     /* de enteros a numeros romanos */
     String toRoman(int numeral){
-        //TODO segunda parte
+        def NUMERAL_TO_ROMAN_MAP = [1000:"M", 900:"CM", 500:"D", 400:"CD", 100:"C", 90:"XC", 50:"L", 40:"XL", 10:"X", 9:"IX", 5:"V", 4:"IV", 1:"I"]
+        String roman = ""
+        
+        while(numeral > 0){
+            def descendantSortedKeys = NUMERAL_TO_ROMAN_MAP.keySet().sort().reverse()
+            def greaterDigit = descendantSortedKeys.find { it <= numeral }
+            numeral -= greaterDigit
+            roman += NUMERAL_TO_ROMAN_MAP[greaterDigit]
+        }
+        
+        roman
     }
 }
