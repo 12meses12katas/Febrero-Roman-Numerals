bbc77b1a78837c3e6385fd60975640f3296a7a23
Andrés Maneiro <andres.maneiro@gmail.com>
2011-03-21 03:07:05 +0800

    Roman2Numeric parser

--- amaneiro/roman_numerals.py
+++ amaneiro/roman_numerals.py
@@ -31,3 +31,39 @@ class NumericToRomanParser:
         else:
             return 'I'*number
 
+class RomanToNumericParser:
+
+    def __init__(self):
+        self.rosettaStone = {
+            'I':1,
+            'V':5,
+            'X':10,
+            'L':50,
+            'C':100,
+            'D':500,
+            'M':1000
+            }
+
+    def getNumericNumber(self, roman):
+        roman = roman.upper()
+        if roman in self.rosettaStone.keys():
+            return self.rosettaStone[roman]
+        elif 'M' in roman:
+            return 1000*roman.count('M') + \
+                self.getNumericNumber(roman.split('M')[-1])
+        elif 'D' in roman:
+            return 500 + \
+                self.getNumericNumber(roman.split('D')[-1])
+        elif 'C' in roman:
+            return 100*roman.count('C') + \
+                self.getNumericNumber(roman.split('C')[-1])
+        elif 'L' in roman:
+            return 50+self.getNumericNumber(roman.split('L')[-1])
+        elif 'X' in roman:
+            return 10*roman.count('X') + \
+                self.getNumericNumber(roman.split('X')[-1])
+        elif 'V' in roman:
+            return 5 + \
+                self.rosettaStone['I']*(len(roman)-1)
+        else:
+            return self.rosettaStone['I']*len(roman)
